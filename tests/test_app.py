9b5de84c334cbf3ead6ee55cff8c8f6e0dbd3acc
Samuel Hargreaves <samjhargreaves@gmail.com>
2026-01-20 16:55:10 +0000

    first_tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,12 +5,22 @@ from playwright.sync_api import Page, expect
 """
 We can render the index page
 """
-def test_get_index(page, test_web_address):
+def test_get_page_title(page, test_web_address):
     # We load a virtual browser and navigate to the /index page
-    page.goto(f"http://{test_web_address}/index")
+    page.goto(f"http://{test_web_address}")
 
     # We look at the <p> tag
-    p_tag = page.locator("p")
+    p_tag = page.locator("a")
 
     # We assert that it has the text "This is the homepage."
-    expect(p_tag).to_have_text("This is the homepage.")
+    expect(p_tag).to_have_text("makersbnb")
+
+def test_header_available_homes(page, test_web_address):
+    # We load a virtual browser and navigate to the /index page
+    page.goto(f"http://{test_web_address}")
+
+    # We look at the <p> tag
+    p_tag = page.locator("h2")
+
+    # We assert that it has the text "This is the homepage."
+    expect(p_tag).to_have_text("Available Homes")
