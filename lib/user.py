e04f6cd2c2732ba8637f9c83c940088020f8a2c6
Charlotte Gale <CharlotteMGale@outlook.com>
2026-01-21 13:41:58 +0000

    test drives basic create logic, no hashing

--- lib/user.py
+++ lib/user.py
@@ -4,4 +4,9 @@ class User:
         self.email = email
         self.password = password
         self.name = name
-        
+
+    def __repr__(self):
+        return f"User({self.id}, {self.email}, {self.password}, {self.name})"
+    
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
--- tests/test_user.py
+++ tests/test_user.py
@@ -10,3 +10,20 @@ def test_user_constructs():
     assert user.email == 'test@email.com'
     assert user.password == 'TestPassword!'
     assert user.name == 'Test User'
+
+"""
+Ensure User object stringifies
+"""
+def test_user_stringifies():
+    user = User(1, 'test@email.com', 'TestPassword!', 'Test User')
+
+    assert str(user) == "User(1, test@email.com, TestPassword!, Test User)"
+
+"""
+Ensure 2 identical User objects are equal
+"""
+def test_identical_users_are_equal():
+    user1 = User(1, 'test@email.com', 'TestPassword!', 'Test User')
+    user2 = User(1, 'test@email.com', 'TestPassword!', 'Test User')
+
+    assert user1 == user2
