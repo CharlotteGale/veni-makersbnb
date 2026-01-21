f3d688f73fb8bc0a54df0f46970f1dc843bac70e
williamharrison <will.harri12@gmail.com>
2026-01-21 12:18:58 +0000

    use real database listings

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-import os
 from flask import Flask, request, render_template
-from lib.database_connection import get_flask_database_connection
+from lib.database_connection import DatabaseConnection
+from lib.listing_repository import ListingRepository
 
-# Create a new Flask app
 app = Flask(__name__)
+
+
+connection = DatabaseConnection(test_mode=False)
+connection.connect()
+connection.seed("seeds/makersbnb_veni.sql")
+
+repository = ListingRepository(connection)
+
 
 # == Your Routes Here ==
 
@@ -21,52 +28,22 @@ app = Flask(__name__)
 # if __name__ == '__main__':
 #     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
 
-class FakeListing:
-    def __init__(self, name, description, price_per_night):
-        self.name = name
-        self.description = description
-        self.price_per_night = price_per_night
-
 
 @app.route("/")
 def index():
-    fake_listings = [
-        {
-            "name": "Cozy Flat",
-            "description": "Close to city centre",
-            "price_per_night": 95,
-            "image_filename": "flat1.jpg"
-        },
-        {
-            "name": "Beach House",
-            "description": "Sea views",
-            "price_per_night": 150,
-            "image_filename": None
-        },
-                {
-            "name": "Cozy Flat",
-            "description": "Close to city centre",
-            "price_per_night": 95,
-            "image_filename": "flat1.jpg"
-        },
-        {
-            "name": "Beach House",
-            "description": "Sea views",
-            "price_per_night": 150,
-            "image_filename": None
-        },
-    ]
-
     page = int(request.args.get("page", 1))
     per_page = 3
 
+    # Get all listings from the database
+    all_listings = repository.all()
+
+    # Pagination logic
     start = (page - 1) * per_page
     end = start + per_page
-
-    listings = fake_listings[start:end]
+    listings = all_listings[start:end]
 
     has_prev = page > 1
-    has_next = end < len(fake_listings)
+    has_next = end < len(all_listings)
 
     return render_template(
         "index.html",
@@ -75,6 +52,7 @@ def index():
         has_prev=has_prev,
         has_next=has_next
     )
+
 
 
 
