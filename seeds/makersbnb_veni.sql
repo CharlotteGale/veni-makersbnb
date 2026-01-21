8e9886354304640eaec781c943d460046ae06666
Kirti Sharma <kirtipdk.sharma@gmail.com>
2026-01-21 13:59:49 +0000

    Adding find functionality to the listing repo, find on id and wildcard find on name

--- seeds/makersbnb_veni.sql
+++ seeds/makersbnb_veni.sql
@@ -15,7 +15,8 @@ INSERT INTO listings (user_id, name, description, price_per_night) VALUES
     (2, 'Shoreditch Loft Apartment', 'Trendy open-plan loft in the heart of Shoreditch, minutes from coffee spots, nightlife, and the Tube.', 160),
     (3, 'Countryside Barn Retreat', 'Peaceful converted barn with countryside walks, a wood burner, and beautiful sunset views.', 140),
     (2, 'Luxury City Penthouse', 'Modern penthouse with skyline views, floor-to-ceiling windows, balcony seating, and premium finishes.', 320),
-    (4, 'Budget-Friendly Private Room', 'Simple private room in a shared flat with great transport links — clean, safe, and ideal for solo travellers.', 55);
+    (4, 'Budget-Friendly Private Room', 'Simple private room in a shared flat with great transport links — clean, safe, and ideal for solo travellers.', 55),
+    (1, 'Trendy Shoreditch Studio', 'Awesome space near coffee shops and bars, great for experiencing this part of london in all its hipster glory', 145);
 
 
 CREATE TYPE booking_status AS ENUM ('pending', 'confirmed', 'rejected');
--- tests/test_listing_repository.py
+++ tests/test_listing_repository.py
@@ -14,7 +14,8 @@ def test_get_all_records(db_connection):
         Listing(2, 2, 'Shoreditch Loft Apartment', 'Trendy open-plan loft in the heart of Shoreditch, minutes from coffee spots, nightlife, and the Tube.', 160),
         Listing(3, 3, 'Countryside Barn Retreat', 'Peaceful converted barn with countryside walks, a wood burner, and beautiful sunset views.', 140),
         Listing(4, 2, 'Luxury City Penthouse', 'Modern penthouse with skyline views, floor-to-ceiling windows, balcony seating, and premium finishes.', 320),
-        Listing(5, 4, 'Budget-Friendly Private Room', 'Simple private room in a shared flat with great transport links — clean, safe, and ideal for solo travellers.', 55)
+        Listing(5, 4, 'Budget-Friendly Private Room', 'Simple private room in a shared flat with great transport links — clean, safe, and ideal for solo travellers.', 55),
+        Listing(6, 1, 'Trendy Shoreditch Studio', 'Awesome space near coffee shops and bars, great for experiencing this part of london in all its hipster glory', 145)
     ]
 
 
@@ -29,4 +30,43 @@ def test_create_new_record(db_connection):
 
     new_listing = repo.create(Listing(None, 1, 'Test Name', 'Test Description', 15))
 
-    assert new_listing == Listing(6, 1, 'Test Name', 'Test Description', 15)
+    assert new_listing == Listing(7, 1, 'Test Name', 'Test Description', 15)
+
+
+"""
+When we call ListingRepository#find
+We get a single Listing object reflecting the seed data.
+"""
+
+def test_get_single_record(db_connection): 
+    db_connection.seed("seeds/makersbnb_veni.sql")
+    repo = ListingRepository(db_connection)
+    listing = repo.find(2)
+    assert listing == Listing(2, 2, 'Shoreditch Loft Apartment', 'Trendy open-plan loft in the heart of Shoreditch, minutes from coffee spots, nightlife, and the Tube.', 160)
+
+"""
+When we call ListingRepository#search_by_name
+We get listings where the name partially matches the search keyword.
+"""
+
+def test_search_by_name_returns_matching_listings(db_connection):
+        db_connection.seed("seeds/makersbnb_veni.sql")
+        repo = ListingRepository(db_connection)
+
+        search_results = repo.search_by_name('Shoreditch')
+
+        assert search_results == [
+                Listing(2, 2, 'Shoreditch Loft Apartment',
+                'Trendy open-plan loft in the heart of Shoreditch, minutes from coffee spots, nightlife, and the Tube.',
+                160),
+                Listing(6, 1, 'Trendy Shoreditch Studio', 'Awesome space near coffee shops and bars, great for experiencing this part of london in all its hipster glory', 145)]
+
+
+"""
+When we call ListingRepository#delete
+The listing record is deleted from the database.
+"""
+
+# def test_delete_listing():
+#         db_connection.seed("seeds/makersbnb_veni.sql")
+#         repo = ListingRepository(db_connection)
