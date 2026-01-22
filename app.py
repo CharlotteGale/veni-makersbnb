8b3b086cbcaf10ef0456920618cfb153759c4223
Tariq <tariqali87b@gmail.com>
2026-01-22 13:47:41 +0000

    attempt to link to databse

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, render_template
-from lib.database_connection import get_flask_database_connection
+from lib.database_connection import get_flask_database_connection,  DatabaseConnection
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -27,6 +27,6 @@ if __name__ == '__main__':
     # In production we don't want the fancy error messages — users won't know
     # what to do with them. So no `debug=True`
     if os.environ.get("APP_ENV") == "PRODUCTION":
-        app.run(port=5001, host='0.0.0.0')
+        app.run(port=5002, host='0.0.0.0')
     else:
-        app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+        app.run(debug=True, port=int(os.environ.get('PORT', 5002)))
